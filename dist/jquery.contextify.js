fbbb0de2e008c937924c9d157db7f8ba189b9f63
koding user <adam@abemedia.co.uk>
2015-02-24 05:09:12 +0000

    v1.0.5

--- dist/jquery.contextify.js
+++ dist/jquery.contextify.js
@@ -21,7 +21,8 @@
     var pluginName = 'contextify',
         defaults = {
             items: [],
-            menuId: "contextify-menu"
+            menuId: "contextify-menu",
+            before: false
         },
         contextifyId = 0;
         
@@ -39,68 +40,78 @@
     Plugin.prototype.init = function () {
         var options = $.extend( {}, this.options, $(this.element).data());
         options.id = contextifyId;
-    
+        
         $(this.element)
-        .attr('data-contextify-id', options.id)
-        .on('contextmenu', function (e) {
-            e.preventDefault();
-        var menu = $('<ul class="dropdown-menu" role="menu" id="' + options.menuId + '" data-contextify-id="' + options.id + '"/>');
-    
-        menu.data(options);
-        
-        var l = options.items.length;
-        var i;
-        
-        for (i = 0; i < l; i++) {
-            var item = options.items[i];
-            var el = $('<li/>');
-            
-            if (item.divider) {
-                el.addClass('divider');
-            } 
-            else if (item.header) {
-                el.addClass('dropdown-header');
-                el.html(item.header);
-            }
-            else {
-                el.append('<a/>');
-                var a = el.find('a');
+            .attr('data-contextify-id', options.id)
+            .on('contextmenu', function (e) {
+                e.preventDefault();
                 
-                if (item.href) {
-                    a.attr('href', item.href);
+                // run before
+                if(typeof(options.before) === 'function') {
+                    options.before(this, options);
                 }
-                if (item.onclick) {
-                    a.on('click', options, item.onclick);
-                    a.css('cursor', 'pointer');
+                
+                var menu = $('<ul class="dropdown-menu" role="menu" id="' + options.menuId + '" data-contextify-id="' + options.id + '"/>');
+                
+                menu.data(options);
+                
+                var l = options.items.length;
+                var i;
+                
+                for (i = 0; i < l; i++) {
+                    var item = options.items[i];
+                    var el = $('<li/>');
+                    
+                    if (item.divider) {
+                        el.addClass('divider');
+                    } 
+                    else if (item.header) {
+                        el.addClass('dropdown-header');
+                        el.html(item.header);
+                    }
+                    else {
+                        el.append('<a/>');
+                        var a = el.find('a');
+                        
+                        if (item.href) {
+                            a.attr('href', item.href);
+                        }
+                        if (item.onclick) {
+                            a.on('click', options, item.onclick);
+                            a.css('cursor', 'pointer');
+                        }
+                        if (item.data) {
+                        for (var data in item.data) {
+                            menu.attr('data-' + data, item.data[data]);
+                        }
+                            a.data(item.data);
+                        }
+                        a.html(item.text);
+                    }
+                    
+                    menu.append(el);
                 }
-                if (item.data) {
-                for (var data in item.data) {
-                    menu.attr('data-' + data, item.data[data]);
+                
+                var currentMenu = $("#" + options.menuId);
+                    
+                if (currentMenu.length > 0) {
+                    if(currentMenu !== menu) {
+                        currentMenu.replaceWith(menu);
+                    }
+                } 
+                else {
+                    $('body').append(menu);
                 }
-                    a.data(item.data);
-                }
-                a.html(item.text);
-            }
-            
-            menu.append(el);
-        }
-            
-        if ($("#" + options.menuId).length > 0) {
-            $("#" + options.menuId).replaceWith(menu);
-        } 
-        else {
-            $('body').append(menu);
-        }
-        
-        var clientTop = $(window).scrollTop() + e.clientY,
-            x = (menu.width() + e.clientX < $(window).width()) ? e.clientX : e.clientX - menu.width(),
-            y = (menu.height() + e.clientY < $(window).height()) ? clientTop : clientTop - menu.height();
-        
-        menu
-            .css('top', y)
-            .css('left', x)
-            .show();
-        })
+                
+                var clientTop = $(window).scrollTop() + e.clientY,
+                    x = (menu.width() + e.clientX < $(window).width()) ? e.clientX : e.clientX - menu.width(),
+                    y = (menu.height() + e.clientY < $(window).height()) ? clientTop : clientTop - menu.height();
+                
+                menu
+                    .css('top', y)
+                    .css('left', x)
+                    .show();
+            })
         .parents().on('mouseup', function () {
             $("#" + options.menuId).hide();
         });
@@ -125,7 +136,10 @@
     
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
-            if (!$.data(this, 'plugin_' + pluginName)) {
+            if( $.data(this, 'plugin_' + pluginName) && Object.prototype.toString.call(options) === '[object String]' ) {
+                $.data(this, 'plugin_' + pluginName)[options]();
+            }
+            else if (!$.data(this, 'plugin_' + pluginName)) {
                 $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
             }
         });
